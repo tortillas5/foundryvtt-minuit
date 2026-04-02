4afc208ffb9db745c8b9f09cd2a3ee1638c31585
Théo Zehnder <tzehnder@tradim.com>
2026-04-02 14:22:01 +0200

    Updated to version 12 of foundry VTT.
    We don't use deprecated functions/helpers anymore.
    Updated system.json file.

--- module/item/item-sheet.js
+++ module/item/item-sheet.js
@@ -6,7 +6,7 @@ export class MinuitItemSheet extends ItemSheet {
 
   /** @override */
   static get defaultOptions() {
-    return mergeObject(super.defaultOptions, {
+    return foundry.utils.mergeObject(super.defaultOptions, {
       classes: ["minuit", "sheet", "item"],
       width: 520,
       height: 480,
