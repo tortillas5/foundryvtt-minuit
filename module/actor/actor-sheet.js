a7041bc180fed91361e394498280a7338ba7f685
Théo Zehnder <tzehnder@tradim.com>
2026-04-10 13:46:59 +0200

    wip: Déplacer les méthodes spécifiques dans la classe spécifique.

--- module/actor/actor-sheet.js
+++ module/actor/actor-sheet.js
@@ -30,7 +30,6 @@ export class MinuitActorSheet extends HandlebarsApplicationMixin(ActorSheetV2) {
       tensionMinus: MinuitActorSheet._onTensionMinus,
       coche: MinuitActorSheet._onCoche,
       roll: MinuitActorSheet._onRoll,
-      useParticularity: MinuitActorSheet._onUseParticularity,
     },
     form: {
       submitOnChange: true,
@@ -163,54 +162,6 @@ export class MinuitActorSheet extends HandlebarsApplicationMixin(ActorSheetV2) {
     });
   }
 
-  /**
-   * Utiliser une particularité.
-   * Dans le HTML : <a data-action="useParticularity" data-item-id="...">
-   */
-  static async _onUseParticularity(event, target) {
-    event.preventDefault();
-    const itemId = target.dataset.itemId;
-
-    const particularite = this.actor.items.find(
-      i => i.type === "particularite" && i.id === itemId
-    );
-    if (!particularite) return;
-
-    const currentAdrenaline = this.actor.system.adrenaline.value;
-    const adrenalineMax     = this.actor.system.adrenaline.max;
-    let message;
-
-    if (particularite.system.type === "force") {
-      if (currentAdrenaline >= particularite.system.value) {
-        const newAdrenaline = currentAdrenaline - particularite.system.value;
-        await this.actor.update({ "system.adrenaline.value": newAdrenaline });
-        message = particularite.name +
-          game.i18n.format("MINUIT.Messages.particularite_usage_used_adrenaline", {
-            value: particularite.system.value,
-          });
-      } else {
-        message = particularite.name +
-          game.i18n.localize("MINUIT.Messages.particularite_usage_out_of_adrenaline");
-      }
-    } else if (particularite.system.type === "faiblesse") {
-      const newAdrenaline = Math.min(
-        currentAdrenaline + particularite.system.value,
-        adrenalineMax
-      );
-      await this.actor.update({ "system.adrenaline.value": newAdrenaline });
-      message = particularite.name +
-        game.i18n.format("MINUIT.Messages.particularite_usage_added_adrenaline", {
-          value: particularite.system.value,
-        });
-    }
-
-    ChatMessage.create({
-      speaker: ChatMessage.getSpeaker({ actor: this.actor }),
-      flavor:  game.i18n.localize("MINUIT.Messages.particularite_usage"),
-      content: message,
-    });
-  }
-
   getTypeName(type) {
     const key = {
       arme:         "MINUIT.Type.arme",
@@ -237,31 +188,4 @@ export class MinuitActorSheet extends HandlebarsApplicationMixin(ActorSheetV2) {
 
     return game.i18n.localize(key);
   }
-}
-
-// 221b-baker-street-sheet.js
-export class Minuit221bActorSheet extends MinuitActorSheet {
-  static PARTS = {
-    sheet: {
-      template: "systems/minuit/templates/actor/221b-baker-street-sheet.html"
-    }
-  };
-}
-
-// 221b-baker-street-sheet.js
-export class MinuitCharacterActorSheet extends MinuitActorSheet {
-  static PARTS = {
-    sheet: {
-      template: "systems/minuit/templates/actor/221b-baker-street-sheet.html"
-    }
-  };
-}
-
-// 221b-baker-street-sheet.js
-export class MinuitDirectriceActorSheet extends MinuitActorSheet {
-  static PARTS = {
-    sheet: {
-      template: "systems/minuit/templates/actor/221b-baker-street-sheet.html"
-    }
-  };
 }
--- module/actor/character-sheet.js
+++ module/actor/character-sheet.js
@@ -20,4 +20,63 @@ export class MinuitCharacterActorSheet extends MinuitActorSheet {
   tabGroups = {
     primary: "description",
   };
+
+  /**
+   * Configuration.
+   * 
+   * @override
+   */
+  static DEFAULT_OPTIONS = {
+    actions: {
+      useParticularity: MinuitCharacterActorSheet._onUseParticularity,
+    },
+  };
+
+    /**
+   * Utiliser une particularité.
+   * Dans le HTML : <a data-action="useParticularity" data-item-id="...">
+   */
+  static async _onUseParticularity(event, target) {
+    event.preventDefault();
+    const itemId = target.dataset.itemId;
+
+    const particularite = this.actor.items.find(
+      i => i.type === "particularite" && i.id === itemId
+    );
+    if (!particularite) return;
+
+    const currentAdrenaline = this.actor.system.adrenaline.value;
+    const adrenalineMax     = this.actor.system.adrenaline.max;
+    let message;
+
+    if (particularite.system.type === "force") {
+      if (currentAdrenaline >= particularite.system.value) {
+        const newAdrenaline = currentAdrenaline - particularite.system.value;
+        await this.actor.update({ "system.adrenaline.value": newAdrenaline });
+        message = particularite.name +
+          game.i18n.format("MINUIT.Messages.particularite_usage_used_adrenaline", {
+            value: particularite.system.value,
+          });
+      } else {
+        message = particularite.name +
+          game.i18n.localize("MINUIT.Messages.particularite_usage_out_of_adrenaline");
+      }
+    } else if (particularite.system.type === "faiblesse") {
+      const newAdrenaline = Math.min(
+        currentAdrenaline + particularite.system.value,
+        adrenalineMax
+      );
+      await this.actor.update({ "system.adrenaline.value": newAdrenaline });
+      message = particularite.name +
+        game.i18n.format("MINUIT.Messages.particularite_usage_added_adrenaline", {
+          value: particularite.system.value,
+        });
+    }
+
+    ChatMessage.create({
+      speaker: ChatMessage.getSpeaker({ actor: this.actor }),
+      flavor:  game.i18n.localize("MINUIT.Messages.particularite_usage"),
+      content: message,
+    });
+  }
 }
