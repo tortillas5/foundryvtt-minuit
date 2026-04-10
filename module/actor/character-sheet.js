207bf987617785351541c5bfa9c8642f836d8167
Théo Zehnder <tzehnder@tradim.com>
2026-04-10 13:33:41 +0200

    wip: La fiche de personnage est passée en AppV2.

--- module/actor/character-sheet.js
+++ module/actor/character-sheet.js
@@ -1,5 +1,23 @@
 import { MinuitActorSheet } from "./actor-sheet.js";
 
 export class MinuitCharacterActorSheet extends MinuitActorSheet {
-  static PARTS = { sheet: { template: "systems/minuit/templates/actor/character-sheet.html" } };
+  static PARTS = {
+    sheet: { template: "systems/minuit/templates/actor/character-sheet.html" }
+  };
+
+  static TABS = {
+    primary: {
+      tabs: [
+        { id: "description" },
+        { id: "expertises" },
+        { id: "particularite" },
+        { id: "equipement" },
+      ],
+      initial: "description",
+    },
+  };
+
+  tabGroups = {
+    primary: "description",
+  };
 }
