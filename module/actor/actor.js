8371e3f6e2f60d7bcee7e6b4c0bb2d40d294061d
Théo Zehnder <tzehnder@tradim.com>
2026-04-13 13:28:57 +0200

    wip: Refacto actor.js

--- module/actor/actor.js
+++ module/actor/actor.js
@@ -7,32 +7,47 @@ export class MinuitActor extends Actor {
   /**
    * Augment the basic actor data with additional dynamic data.
    */
-  prepareData() {
-    super.prepareData();
+  prepareDerivedData() {
+    super.prepareDerivedData();
 
-    const actor = this;
-    const system = actor.system;
-    const items = actor.items;
-    
-    system.possessions = items.filter(item => item.type === "possession").sort((a, b) => a.name.localeCompare(b.name));
+    const system = this.system;
+    const items = this.items;
 
-    // Make separate methods for each Actor type (character, npc, etc.) to keep
-    // things organized.
-    if (actor.type === 'character') this._prepareCharacterData(actor);
-    if (actor.type === '221b-baker-street') this._prepareCabinetData(actor);
+    system.possessions = items
+      .filter(item => item.type === "possession")
+      .sort((a, b) => a.name.localeCompare(b.name));
+
+    switch (this.type) {
+      case "character":
+        this._prepareCharacterData();
+        break;
+
+      case "221b-baker-street":
+        this._prepareCabinetData();
+        break;
+    }
   }
 
   /**
    * Prepare Character type specific data
    */
-  _prepareCharacterData(actor) {
-    const system = actor.system;
-    const items = actor.items;
-    system.armes = items.filter(item => item.type === "arme").sort((a, b) => a.name.localeCompare(b.name));
-    system.forces = items.filter(item => item.type === "particularite" && item.system.type==="force").sort((a, b) => a.name.localeCompare(b.name));
-    system.faiblesses = items.filter(item => item.type === "particularite" && item.system.type==="faiblesse").sort((a, b) => a.name.localeCompare(b.name));
+  _prepareCharacterData() {
+    const system = this.system;
+    const items = this.items;
 
-    for (let [key, aspect] of Object.entries(system.aspects)) {
+    system.armes = items
+      .filter(item => item.type === "arme")
+      .sort((a, b) => a.name.localeCompare(b.name));
+
+    system.forces = items
+      .filter(item => item.type === "particularite" && item.system.type==="force")
+      .sort((a, b) => a.name.localeCompare(b.name));
+
+    system.faiblesses = items
+      .filter(item => item.type === "particularite" && item.system.type==="faiblesse")
+      .sort((a, b) => a.name.localeCompare(b.name));
+
+    for (const [key, aspect] of Object.entries(system.aspects)) {
       aspect.nom = game.i18n.localize(`MINUIT.Aspects.${key}`);
     }
   }
@@ -40,23 +55,28 @@ export class MinuitActor extends Actor {
   /**
    * Prepare Cabinet type specific data
    */
-  _prepareCabinetData(actor) {
-    const system = actor.system;
-    const items = actor.items;
-    system.historiques = items.filter(item => item.type === "historique").sort((a, b) => a.name.localeCompare(b.name));
-    system.contacts = items.filter(item => item.type === "contact").sort((a, b) => a.system.categorie.localeCompare(b.system.categorie) || a.name.localeCompare(b.name));
+  _prepareCabinetData() {
+    const system = this.system;
+    const items = this.items;
 
-    for (let [key, contact] of Object.entries(system.contacts)) {
+    system.historiques = items
+      .filter(item => item.type === "historique")
+      .sort((a, b) => a.name.localeCompare(b.name));
+
+    system.contacts = items
+      .filter(item => item.type === "contact")
+      .sort((a, b) => a.system.categorie.localeCompare(b.system.categorie) || a.name.localeCompare(b.name));
+
+    for (const [key, contact] of Object.entries(system.contacts)) {
       contact.categorieDesc = game.i18n.localize(`MINUIT.CategorieInfluence.${contact.system.categorie}`);
     }
   }
 
-  static async create(system, options)
-  {
-    if (system.type=="221b-baker-street")
-      system.img = "systems/minuit/images/221b.webp";
+  static async create(data, options) {
+    if (data.type === "221b-baker-street" && !data.img) {
+      data.img = "systems/minuit/images/221b.webp";
+    }
 
-    super.create(system, options);
+    return super.create(data, options);
   }
-
 }
